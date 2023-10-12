0e93ea2f2482c271b514e538451be86ecad06213
DevLeti <devleti0106@gmail.com>
2023-10-12 14:10:14 +0900

    EDIT models.py: delete __str__

--- User/models.py
+++ User/models.py
@@ -6,8 +6,8 @@ class User(models.Model):
     user_id = models.CharField(max_length=200, primary_key=True)
     user_pw = models.CharField(max_length=200)
 
-    def __str__(self):
-        return self.user_id + " " + self.user_pw
+    # def __str__(self):
+    #     return self.user_id + " " + self.user_pw
 
 # TODO: Server CRUD 구현
 class Server(models.Model):
@@ -15,9 +15,6 @@ class Server(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     server_name = models.CharField(max_length=200)
     server_url = models.CharField(max_length=400)
-
-    def __str__(self):
-        return str(self.server_id) + " " + self.server_name
 
 # TODO: python manage.py makemigration & migrate
 # TODO: Tag CRUD 구현
@@ -30,6 +27,3 @@ class Tag(models.Model):
 class Like(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     server_id = models.ForeignKey(Server, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.user_id + " " + self.server_id
