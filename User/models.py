c9f963d3d9e555e30f1c676903612b23b475d6cc
DevLeti <devleti0106@gmail.com>
2023-10-12 14:43:52 +0900

    EDIT TODO

--- User/models.py
+++ User/models.py
@@ -9,14 +9,12 @@ class User(models.Model):
     # def __str__(self):
     #     return self.user_id + " " + self.user_pw
 
-# TODO: Server CRUD 구현
 class Server(models.Model):
     server_id = models.AutoField(primary_key=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     server_name = models.CharField(max_length=200)
     server_url = models.CharField(max_length=400)
 
-# TODO: python manage.py makemigration & migrate
 # TODO: Tag CRUD 구현
 class Tag(models.Model):
     server_id = models.ForeignKey(Server, on_delete=models.CASCADE)
