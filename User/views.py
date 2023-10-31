b333dfbca559804af73f1a517f82c6934f450a5f
DevLeti <devleti0106@gmail.com>
2023-10-31 22:26:10 +0900

    ADD JWT

--- User/views.py
+++ User/views.py
@@ -6,6 +6,8 @@ from rest_framework.views import APIView
 from .serializers import UserSerializer, ServerSerializer, LikeSerializer, TagSerializer
 from rest_framework import status
 from django.http import Http404
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 class UserListAPI(APIView):
     def get(self, request):
@@ -170,3 +172,17 @@ class LikeDetailAPI(APIView):
     #         serializer.save()
     #         return Response(serializer.data)
     #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        # ...
+
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
