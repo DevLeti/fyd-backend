98802a64e4050b462cf9cb6580c374bb5798fdd0
DevLeti <devleti0106@gmail.com>
2023-11-01 23:19:58 +0900

    EDIT: delete completed TODO, erase unused code

--- User/models.py
+++ User/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser,User
 
-# class User(AbstractUser):
-#     user_id = models.CharField(max_length=200)
-#     user_pw = models.CharField(max_length=200)
-
 class Server(models.Model):
     server_id = models.AutoField(primary_key=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -12,7 +8,6 @@ class Server(models.Model):
     server_url = models.CharField(max_length=400)
     server_description = models.CharField(max_length=4000, null=True, blank=True)
 
-# TODO: Tag CRUD 구현
 class Tag(models.Model):
     server_id = models.ForeignKey(Server, on_delete=models.CASCADE)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
