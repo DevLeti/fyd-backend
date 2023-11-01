ff7f23e022f8f401b9bedefab3f0e6197bb48ce0
DevLeti <devleti0106@gmail.com>
2023-11-01 17:02:12 +0900

    ADD: Swagger

--- fyd/urls.py
+++ fyd/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.conf import settings
 from User.views import UserListAPI, UserDetailAPI, ServerListAPI, ServerDetailAPI, LikeListAPI, LikeDetailAPI, TagListAPI, TagDetailAPI, MyTokenObtainPairView, RegisterView
 from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -32,3 +36,22 @@ urlpatterns = [
     path('api/like/', LikeListAPI.as_view()),
     path('api/like/<int:pk>/', LikeDetailAPI.as_view())
 ]
+
+schema_view = get_schema_view( 
+    openapi.Info( 
+        title="FYI API", 
+        default_version="v1", 
+        description="Fit Your Discord API Document", 
+        terms_of_service="https://www.google.com/policies/terms/", 
+        contact=openapi.Contact(name="test", email="test@test.com"), 
+        license=openapi.License(name="Test License"), 
+    ), 
+    public=True, 
+    permission_classes=(permissions.AllowAny,), 
+)
+
+if settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name="schema-json"),
+        re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+        re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),    ]
